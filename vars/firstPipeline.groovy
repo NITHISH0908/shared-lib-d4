0bc8c48b99b0a97d17de745f7ea99d3e2e6f0943
NITHISH0908 <88084587+NITHISH0908@users.noreply.github.com>
2026-07-07 20:00:18 +0530

    added env varisble

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -3,6 +3,8 @@
 
 import com.i27academy.builds.Calculator;
 
+def call(Map pipelineparams){
+
 // An instance of class called Calculator is created
 
 Calculator calculator = new Calculator(this)
@@ -11,16 +13,24 @@ Calculator calculator = new Calculator(this)
 
 pipeline{
     agent any{
+        environment{
+            APP_NAME = ${pipelineparams.appName} //this value should be coming from microservices
+        }
         stages{
             stage('AdditionStage'){
                 steps{
                     script{
                         echo "Printing sum of 2 numbers"
                         println calculator.add(3,4)
+                        echo "**** MicroService name is  ${APP_NAME}****"
                     }
                 }
             }
         }
+        stage('Secondstage'){
+            echo "executing the second stage"
+        }
     }
+}
 }
 
